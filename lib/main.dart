3430debb8261eba091081b64b38a3a050d721997
Nassem <nassemmubarak@gmail.com>
2022-06-14 13:53:37 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("nassem Mubarak"),
+          actions: const [
+            Icon(Icons.fingerprint),
+          ],
+        ),
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: const [
+                Text(
+                  "Nassem Ahmed mubarak",
+                  style: TextStyle(
+                    color: Colors.red,
+                    backgroundColor: Colors.amber,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 50
+                  ),
+                  textAlign: TextAlign.center,
+
+                ),
+                Text(
+                  'Nassem Wellcom',
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
